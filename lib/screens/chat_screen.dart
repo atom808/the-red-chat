9f79313f8f74869794d22e199842c8238ad09bd1
Diego Pestana <diegopestana808@gmail.com>
2019-11-28 11:29:08 -0300

    DoubleTap nao esta funcionando

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_chat_ui_starter/Widgets/category_selector.dart';
-import 'package:flutter_chat_ui_starter/Widgets/frequently_contacted.dart';
-import 'package:flutter_chat_ui_starter/Widgets/message_list.dart';
 import 'package:flutter_chat_ui_starter/models/message_model.dart';
 import 'package:flutter_chat_ui_starter/models/user_model.dart';
 
 class ChatScreen extends StatefulWidget {
   final User user;
   final Message message;
+
   ChatScreen({this.user, this.message});
 
   @override
@@ -16,73 +14,84 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   _buildMessage(Message message, bool isMe){
-    final Container msg = Container(
-      margin:
-      isMe ?
-      EdgeInsets.only(
-        left: 56.0,
-        top: 8.0,
-        bottom: 8.0,
-      ) :
-      EdgeInsets.only(
-        top: 8.0,
-        bottom: 8.0,
-      ),
-      padding: EdgeInsets.symmetric(
-          horizontal: 25.0,
-          vertical: 15.0
-      ),
-      width: MediaQuery.of(context).size.width * 0.75,
-      decoration: BoxDecoration(
-          color: isMe ? Colors.red[50]: Colors.grey[100],
-          borderRadius:
-          isMe ?
-          BorderRadius.only(
-            topLeft: Radius.circular(30.0),
-            bottomLeft: Radius.circular(30.0),
-          ) :
-          BorderRadius.only(
-            topRight: Radius.circular(30.0),
-            bottomRight: Radius.circular(30.0),
-          )
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            message.time,
-            style: TextStyle(
-              fontSize: 12.0,
-              color: Colors.grey[500],
+    /*bool _liked = message.isLiked;*/
+    final InkWell msg = InkWell(
+      onDoubleTap: () => print("funcionou"),
+      child: Container(
+        margin:
+        isMe ?
+        EdgeInsets.only(
+          left: 56.0,
+          top: 8.0,
+          bottom: 8.0,
+        ) :
+        EdgeInsets.only(
+          top: 8.0,
+          bottom: 8.0,
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 24.0,
+          vertical: 12.0,
+        ),
+        width: MediaQuery.of(context).size.width * 0.75,
+        decoration: BoxDecoration(
+            color: isMe ? Colors.red[50]: Colors.grey[100],
+            borderRadius:
+            isMe ?
+            BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              bottomLeft: Radius.circular(30.0),
+            ) :
+            BorderRadius.only(
+              topRight: Radius.circular(30.0),
+              bottomRight: Radius.circular(30.0),
+            )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  message.time,
+                  style: TextStyle(
+                    fontSize: 12.0,
+                    color: Colors.grey[500],
+                  ),
+                ),
+                Icon(
+                  message.isLiked
+                      ? Icons.favorite
+                      : null,
+                  size: 14.0,
+                  color: message.isLiked ? Colors.red : Colors.transparent,
+                ),
+                /*IconButton(
+                icon: message.isLiked
+                    ? Icon(Icons.favorite)
+                    : Icon(null),
+                iconSize: 14.0,
+                color: message.isLiked ? Colors.red : Colors.transparent,
+                onPressed: () {},
+              )*/
+              ],
             ),
-          ),
-          SizedBox(height: 8.0,),
-          Text(
-            message.text,
-            style: TextStyle(
-              fontSize: 18.0,
+            SizedBox(height: 8.0,),
+            Text(
+              message.text,
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
     if(isMe){
      return msg;
     }
-    //Retirar Container de msg para incluir o ícone de coração no container e dar doubleTap pra fav.
-    return Row(
-      children: <Widget>[
-        msg,
-        IconButton(
-          icon: message.isLiked
-              ? Icon(Icons.favorite)
-              : Icon(null),
-          iconSize: 20.0,
-          color: message.isLiked ? Colors.red : Colors.transparent,
-          onPressed: () {},
-        )
-      ],
-    );
+    return msg;
   }
 
   @override
